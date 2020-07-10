92c1dea944c9e00d102aba3e12851e3dafdb02b1
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-07-10 14:23:10 -0300

    feat: added Payment History class in Account

---
+++ src/modules/Account/PaymentHistory.ts
@@ -0,0 +1,49 @@
+import TagoIOModule, { GenericModuleParams } from "../../comum/TagoIOModule";
+import { GenericID, GenericToken } from "../../comum/comum.types";
+
+interface PaymentInfo {
+  status: boolean;
+  result: [
+    {
+      id: GenericID;
+      stripe_id: GenericID;
+      account: GenericID;
+      info: string;
+      notes: string;
+      invoice_number: number;
+      invoice_code: string;
+      total: number;
+      due_date: string;
+      payment_method: string;
+      created_at: string;
+      updated_at: string;
+      paid: true;
+      ref_account: {
+        name: string;
+        email: string;
+        id: GenericID;
+        send_invoice: boolean;
+      };
+      ref_payment_method: { last4: string; brand: string };
+      account_invoice_items: {
+        unit_value: number;
+        description: string;
+        qty: number;
+        value: number;
+      }[];
+    }
+  ];
+}
+
+class PaymentHistory extends TagoIOModule<GenericModuleParams> {
+  public async getHistory(): Promise<PaymentInfo[]> {
+    const result = await this.doRequest<PaymentInfo[]>({
+      path: "/account/payment_history/",
+      method: "GET",
+    });
+
+    return result;
+  }
+}
+
+export default PaymentHistory;
