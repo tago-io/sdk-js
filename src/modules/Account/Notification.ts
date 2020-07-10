e3c2df10e9e40a3416be019700f3ff70acb2fdc4
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-07-10 11:33:51 -0300

    feat: added Notification class in Account

---
+++ src/modules/Account/Notification.ts
@@ -0,0 +1,99 @@
+import TagoIOModule, { GenericModuleParams } from "../../comum/TagoIOModule";
+import { GenericID, GenericToken } from "../../comum/comum.types";
+
+type NotificationType = "dashboard" | "bucket" | "analysis" | "profile" | "tago" | "limit_alert";
+
+type Condition = "None" | "Pending" | "Accepted" | "Refused";
+
+interface NotificationQuery {
+  type: NotificationType;
+  start_date: string;
+  end_date: string;
+  ref_id: GenericID;
+}
+
+interface NotificationInfo {
+  id: GenericID;
+  ref_id: GenericID | null;
+  ref_from: { id: GenericID; name: string };
+  type: NotificationType;
+  sub_type: string;
+  message: string;
+  read: boolean;
+  condition: Condition;
+  created_at: string;
+}
+
+class Notification extends TagoIOModule<GenericModuleParams> {
+  public async list(query?: NotificationQuery): Promise<NotificationInfo[]> {
+    const result = await this.doRequest<NotificationInfo[]>({
+      path: "/notification/",
+      method: "GET",
+      params: query,
+    });
+
+    return result;
+  }
+  public async markAsRead(notificationIDS: GenericID[]): Promise<string> {
+    const result = await this.doRequest<string>({
+      path: "/notification/read",
+      method: "PUT",
+      body: {
+        notification_ids: notificationIDS,
+      },
+    });
+
+    return result;
+  }
+  public async accept(notificationID: GenericID): Promise<string> {
+    const result = await this.doRequest<string>({
+      path: `/notification/accept/${notificationID}`,
+      method: "POST",
+    });
+
+    return result;
+  }
+  public async refuse(notificationID: GenericID): Promise<string> {
+    const result = await this.doRequest<string>({
+      path: `/notification/refuse/${notificationID}`,
+      method: "POST",
+    });
+
+    return result;
+  }
+  public async remove(notificationID: GenericID): Promise<string> {
+    const result = await this.doRequest<string>({
+      path: `/notification/${notificationID}`,
+      method: "DELETE",
+    });
+
+    return result;
+  }
+
+  public async registerDevice(deviceToken: GenericToken, platform: "ios" | "android"): Promise<string> {
+    const result = await this.doRequest<string>({
+      path: `/notification/push/register`,
+      method: "POST",
+      body: {
+        device_token: deviceToken,
+        platform,
+      },
+    });
+
+    return result;
+  }
+
+  public async unRegisterDevice(deviceToken: GenericToken): Promise<string> {
+    const result = await this.doRequest<string>({
+      path: `/notification/push/unregister`,
+      method: "POST",
+      body: {
+        device_token: deviceToken,
+      },
+    });
+
+    return result;
+  }
+}
+
+export default Notification;
