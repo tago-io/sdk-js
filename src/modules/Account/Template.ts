04abf027aa108fa9ee658b8d7586c029bcca4fcd
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-07-13 08:52:25 -0300

    feat: added Template class in Account

---
+++ src/modules/Account/Template.ts
@@ -0,0 +1,47 @@
+import { GenericID } from "../../comum/comum.types";
+import TagoIOModule, { GenericModuleParams } from "../../comum/TagoIOModule";
+
+interface TemplateObj {
+  dashboard: GenericID;
+  name: string;
+  image_logo?: string;
+  image_main?: string;
+  setup?: object;
+}
+
+interface TemplateInstallData {
+  device: { id: GenericID; bucket: GenericID };
+}
+
+class Template extends TagoIOModule<GenericModuleParams> {
+  generateTemplate(template: TemplateObj): Promise<string> {
+    const result = this.doRequest<string>({
+      path: `/template`,
+      method: "POST",
+      body: template,
+    });
+
+    return result;
+  }
+
+  installTemplate(templateID: GenericID, data?: TemplateInstallData): Promise<string> {
+    const result = this.doRequest<string>({
+      path: `/template`,
+      method: "POST",
+      body: data,
+    });
+
+    return result;
+  }
+
+  getTemplate(templateID: GenericID): Promise<object> {
+    const result = this.doRequest<object>({
+      path: `/template/${templateID}`,
+      method: "GET",
+    });
+
+    return result;
+  }
+}
+
+export default Template;
