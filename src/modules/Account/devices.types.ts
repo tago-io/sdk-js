c795e78e4d649753e2984af564c8ee47d9d03142
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-07-22 14:08:49 -0300

    Fix: tags

--- src/modules/Account/devices.types.ts
+++ src/modules/Account/devices.types.ts
@@ -32,7 +32,7 @@ interface DeviceCreateInfo {
   /**
    * An array of tags
    */
-  tags?: TagsObj[];
+  tags?: TagsObj | TagsObj[];
 }
 
 interface DeviceInfo extends Readonly<Omit<DeviceCreateInfo, "configuration_params">> {
