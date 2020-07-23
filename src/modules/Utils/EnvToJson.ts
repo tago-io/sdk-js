16cbcc179e7458e47136a90bc31404376e9bf8dc
Felipe Lima <felipe@ferreiradelima.com>
2020-07-23 16:42:59 -0300

    added test folder and removed groupData

--- src/modules/Utils/EnvToJson.ts
+++ src/modules/Utils/EnvToJson.ts
@@ -5,7 +5,7 @@ import { AnalysisEnvironment } from "../Analysis/analysis.types";
  * Note: It will replace duplicate keys for the last one
  * @param environment Array of environment items from TagoIO
  */
-function envToJson(environment: AnalysisEnvironment[]) {
+function envToJson(environment: AnalysisEnvironment[]): { [key: string]: string } {
   return environment.reduce((pv, cv) => {
     pv[cv.key] = cv.value;
     return pv;
--- src/modules/Utils/Utils.ts
+++ src/modules/Utils/Utils.ts
@@ -1,4 +1,3 @@
 export { default as envToJson } from "./envToJson";
 export { default as getAPIVersion } from "./getAPIVersion";
 export { default as getTokenByName } from "./getTokenByName";
-export { default as groupData } from "./groupData";
