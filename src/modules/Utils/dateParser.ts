d9f8fa09832041273ea94395b248d12d1bddc8c5
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-11-03 09:08:16 -0300

    feat: allow array in datePaser util

--- src/modules/Utils/dateParser.ts
+++ src/modules/Utils/dateParser.ts
@@ -1,25 +1,16 @@
-export default function dateParser<T>(date: T, parameters: (keyof T)[]): T {
-  for (const parameter of parameters) {
-    const value: unknown = date[parameter];
-    if (value && typeof value === "string" && value !== "never") {
-      const parsedDate = new Date(value as string);
-      if (!isNaN(parsedDate.getTime())) {
-        ((date[parameter] as unknown) as Date) = parsedDate;
+export default function dateParser<T>(date: T | T[], parameters: (keyof T)[]): T | T[] {
+  const dateArray = Array.isArray(date) ? date : [date];
+  for (const dateItem of dateArray) {
+    for (const parameter of parameters) {
+      const value: unknown = dateItem[parameter];
+      if (value && typeof value === "string" && value !== "never") {
+        const parsedDate = new Date(value as string);
+        if (!isNaN(parsedDate.getTime())) {
+          ((dateItem[parameter] as unknown) as Date) = parsedDate;
+        }
       }
     }
   }
 
   return date;
 }
-
-// interface test {
-//   a: Date;
-//   b: string;
-// }
-// const asd = { a: "2020-09-28T18:10:21.000Z", b: "never" };
-// console.log(
-//   // @ts-ignore
-//   dateParser<test>(asd, ["a", "b"])
-// );
-
-// console.log(typeof asd.a);
