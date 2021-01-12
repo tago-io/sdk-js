994b3a5cd95c3858eec8e7355edd157995704654
Felipe Lima <felipe@ferreiradelima.com>
2021-01-12 10:43:50 -0300

    export RunUser module

--- src/modules.ts
+++ src/modules.ts
@@ -4,6 +4,7 @@ export { default as Account } from "./modules/Account/Account";
 export { default as Services } from "./modules/Services/Services";
 export { default as Network } from "./modules/Network/Network";
 export { default as Authorization } from "./modules/Authorization/Authorization";
+export { default as RunUser } from "./modules/RunUser/RunUser";
 export { default as Utils } from "./modules/Utils/Utils";
 
 export * as Types from "./types";
