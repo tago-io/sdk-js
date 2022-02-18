62ec405fcb4497e71adf2ba9176868f94de84ccf
Felipe Lima <felipe@ferreiradelima.com>
2022-02-18 16:16:19 -0500

    fix utils export

--- src/modules.ts
+++ src/modules.ts
@@ -5,9 +5,9 @@ export { default as Services } from "./modules/Services/Services";
 export { default as Network } from "./modules/Network/Network";
 export { default as Authorization } from "./modules/Authorization/Authorization";
 export { default as RunUser } from "./modules/RunUser/RunUser";
-export * as Utils from "./modules/Utils/Utils";
 export { default as Dictionary } from "./modules/Dictionary/Dictionary";
 export { default as Migration } from "./modules/Migration/Migration";
 
+export * as Utils from "./modules/Utils/Utils";
 export * as Cache from "./common/Cache";
 export * as Types from "./types";
