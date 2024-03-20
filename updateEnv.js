3985e141a387a785ab4d056fdc1c1e6b1d04e26b
GitHub Action <action@github.com>
2024-03-20 20:25:05 +0000

    generated documentation

--- updateEnv.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const packageJson = require("./package.json");
-
-fs.writeFileSync("./src/infrastructure/envParams.json", `{"version": "${packageJson.version}"}`, {
-  encoding: "utf-8",
-});
